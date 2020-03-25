69add3641070e9a5aeba1ba4c4e0526ba0717189
Tomasz <30973162+tomaszkuehn@users.noreply.github.com>
2020-03-25 23:13:39 +0100

    Create entrypoint.sh

---
+++ myaction/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+pip install --upgrade pip
+pip install cpplint
+
+whereis cpplint
+folders=("include" "includes" "src" "source" "sources" "test" "tests")
+for folder in "${folders[@]}"
+do
+    echo ${folder}
+    if [[ -d ${folder} ]]; then
+        command="cpplint --verbose=0 --recursive ${folder}/*"
+        echo ${command}
+        if ${command} | grep "error"; then
+            echo "::error ::Review your code"
+            
+        fi
+    fi
+done
